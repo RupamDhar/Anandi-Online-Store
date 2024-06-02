2373b7b02acf01b97f7f9b41b4c3baef7a1bf8bd
RupamDhar <rupamdhar1000@gmail.com>
2024-06-02 13:15:47 +0530

    init

--- frontend/src/redux/ReduxStore.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import searchInputReducer from './states/searchInputSlice'
-
-export const store = configureStore({
-    reducer: {
-        setSearchInput: searchInputReducer
-    },
-})
